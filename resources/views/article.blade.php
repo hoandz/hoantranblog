03780a610071939531f43cec69f9169cb9655ace
Hoan Tran <hoantran.us@gmail.com>
2018-09-06 00:33:41 +0700

    commit moi

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,6 +1,20 @@
-@extends('layout.app')
-@section('content')
-<div class="content">
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+        <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+        <link rel="stylesheet" href="{{asset('css/article.css')}}">
+        <link rel="stylesheet" href="{{asset('css/navbar_desktop.css')}}">
+        <link rel="stylesheet" href="{{asset('css/back_to_top.css')}}">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    </head>
+    <body>
+        <div class="content">
     <div class="menu">
         <div class="container">
             <div class="site-branding">
@@ -53,10 +67,66 @@
     <div class="content_post">
         <div class="row">
             <div class="container">
-                <div class="">
+                <div class="row">
+                    <div class="col-md-9 post-body-left">
+                        <div class="title-post">
+                            <span>Phóng to view sử dụng zoom animation trong android</span>
+                        </div>
+                        <div class="article-content">
+                            <p>
+                                Assign image với độ phân giải gốc tới ImageView đc invisible. Ví dụ dưới sẽ load image resource trên UI thread, cách đơn giản nhất.
+                                Tính toán vùng bounds bắt đầu và kết thúc của ImageView.
+                                Dịch chuyển 4 vị trí và kích cở của thuộc tính X, Y, SCALE_X và SCALE_Y từ vùng bound bắt đầu đến vùng bound kết thúc. Sẽ có 4 animation được thêm vào AnimatorSet để chúng có thể start cùng một thời điểm.
+                                Zoom trở về size thumbnail khi user touch vào ImageView hiển thị ảnh lớn.
+                                Assign image với độ phân giải gốc tới ImageView đc invisible. Ví dụ dưới sẽ load image resource trên UI thread, cách đơn giản nhất.
+                                Tính toán vùng bounds bắt đầu và kết thúc của ImageView.
+                                Dịch chuyển 4 vị trí và kích cở của thuộc tính X, Y, SCALE_X và SCALE_Y từ vùng bound bắt đầu đến vùng bound kết thúc. Sẽ có 4 animation được thêm vào AnimatorSet để chúng có thể start cùng một thời điểm.
+                                Zoom trở về size thumbnail khi user touch vào ImageView hiển thị ảnh lớn.
+                                Assign image với độ phân giải gốc tới ImageView đc invisible. Ví dụ dưới sẽ load image resource trên UI thread, cách đơn giản nhất.
+                                Tính toán vùng bounds bắt đầu và kết thúc của ImageView.
+                                Dịch chuyển 4 vị trí và kích cở của thuộc tính X, Y, SCALE_X và SCALE_Y từ vùng bound bắt đầu đến vùng bound kết thúc. Sẽ có 4 animation được thêm vào AnimatorSet để chúng có thể start cùng một thời điểm.
+                                Zoom trở về size thumbnail khi user touch vào ImageView hiển thị ảnh lớn.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 post-body-right">
+                        <div class="row">
+                           <div class="container">
+                                <div class="summary">
+                                    SUMMARY OF CONTENTS
+                                </div>
+                                <div class="summary-content">
+                                    Tính toán vùng bounds bắt đầu và kết thúc của ImageView.
+                                    Dịch chuyển 4 vị trí và kích cở của thuộc tính X, Y, SCALE_X và SCALE_Y từ vùng bound bắt đầu đến vùng bound kết thúc. Sẽ có 4 animation được thêm vào AnimatorSet để chúng có thể start cùng một thời điểm.
+                                    Zoom trở về size thumbnail khi user touch vào ImageView hiển thị ảnh lớn.
+                                </div>
+                           </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="related">
+      <div class="container">
+        <div class="row">
+            <span class="col-md-4" style="margin-top: 20px; font-weight: 700;">Related</span>
+        </div>
+          <div class="row">
+              <div class="col-md-3">
+                  <div class="item"></div>
+              </div>
+              <div class="col-md-3">
+                  <div class="item"></div>
+              </div>
+              <div class="col-md-3">
+                  <div class="item"></div>
+              </div>
+              <div class="col-md-3">
+                  <div class="item"></div>
+              </div>
+          </div>
+      </div>                  
     </div>
     <div class="footer">
         <p>@2018 - Hoantran Blog. All Right Reserved</p>
@@ -74,4 +144,8 @@
     </ul>
 </div>
 <a id="back-to-top" href="#"><i class="fa fa-angle-up"></i></a>
-@stop
+        <script src="{{asset('js/jquery-3.1.1.min.js')}}"></script>
+        <script src="{{asset('js/index.js')}}"></script>
+        <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    </body>
+</html>
